e513eba1cee7d2a4f929a9a239e37f6aa07f3b97
Julian <jm.ord98@gmail.com>
2023-10-16 20:45:28 -0300

    audio view changes

--- src/components/ecgVar/EcgVar.js
+++ src/components/ecgVar/EcgVar.js
@@ -52,60 +52,77 @@ export class EcgVar extends MicroConnected {
         return (
             <div className='col ecgvar'>
                 <div className='row justify-content-around'>
-                    <div className='col-5'>
-                        <div className='card row'>
-                            <h6 className='card-header text-center'>Impedancia</h6>
-                            <div className='card-body'>
-                                <div className='d-flex justify-content-between'>
-                                    <span className='card-text text-secondary'>Valor</span>
-                                    <span className='card-text text-secondary-emphasis'>valor</span>
-                                </div>
-                                <div className='row mt-auto'>
-                                    <div className='col'>
-                                        <button
-                                            type="button"
-                                            className='btn btn-primary'
-                                        //disabled={(!this.state.targetConnected)}
-                                        //onClick={this.reset}
-                                        >
-                                            Calibrar
-                                        </button>
+                    <div className='col-7'>
+                        <div className='row justify-content-center mb-4'>
+                            <div className='col-6'>
+                                <div className='card row'>
+                                    <h6 className='card-header text-center'>Impedancia</h6>
+                                    <div className='card-body'>
+                                        <div className='d-flex justify-content-between'>
+                                            <span className='card-text text-secondary'>Valor</span>
+                                            <span className='card-text text-secondary-emphasis'>valor</span>
+                                        </div>
+                                        <div className='row mt-auto'>
+                                            <div className='col'>
+                                                <button
+                                                    type="button"
+                                                    className='btn btn-primary'
+                                                //disabled={(!this.state.targetConnected)}
+                                                //onClick={this.reset}
+                                                >
+                                                    Calibrar
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                         <div className='card row'>
                             <h6 className='card-header text-center'>Estado</h6>
                             <div className='card-body col'>
                                 <div className='row'>
-                                    <p className='col card-text text-secondary'>Frecuencia cardiaca</p>
-                                    <p className='col card-text text-secondary-emphasis'>valor</p>
-                                    <p className='col card-text text-secondary-emphasis'>Insto</p>
+                                    <p className='col-6 text-start card-text text-secondary'>Frecuencia cardíaca</p>
+                                    <p className='col card-text text-secondary-emphasis'>1</p>
+                                    <div className='col'>
+                                        <p className='card-text badge bg-success'>Insto</p>
+                                    </div>
                                 </div>
                                 <div className='row'>
-                                    <p className='col card-text text-secondary'>Derivación</p>
-                                    <p className='col card-text text-secondary-emphasis'>valor</p>
-                                    <p className='col card-text text-secondary-emphasis'>RA</p>
+                                    <p className='col-6 text-start card-text text-secondary'>Derivación</p>
+                                    <p className='col card-text text-secondary-emphasis'>vaaalor</p>
+                                    <div className='col'>
+                                        <p className='card-text badge bg-success'>RA</p>
+                                    </div>
                                 </div>
                                 <div className='row'>
-                                    <p className='col card-text text-secondary'>Amplitud</p>
+                                    <p className='col-6 text-start card-text text-secondary'>Amplitud</p>
                                     <p className='col card-text text-secondary-emphasis'>valor</p>
-                                    <p className='col card-text text-secondary-emphasis'>LA</p>
+                                    <div className='col'>
+                                        <p className='card-text badge bg-success'>LA</p>
+                                    </div>
                                 </div>
                                 <div className='row'>
-                                    <p className='col card-text text-secondary'>Cant electrodos</p>
+                                    <p className='col-6 text-start card-text text-secondary'>Cant electrodos</p>
                                     <p className='col card-text text-secondary-emphasis'>valor</p>
-                                    <p className='col card-text text-secondary-emphasis'>LL</p>
+                                    <div className='col'>
+                                        <p className='card-text badge bg-danger'>LL</p>
+                                    </div>
                                 </div>
                                 <div className='row'>
-                                    <p className='col card-text text-secondary'>Filtro de linea</p>
+                                    <p className='col-6 text-start card-text text-secondary'>Filtro de linea</p>
                                     <p className='col card-text text-secondary-emphasis'>valor</p>
-                                    <p className='col card-text text-secondary-emphasis'>V</p>
+                                    <div className='col'>
+                                        <p className='card-text badge bg-success'>V</p>
+                                    </div>
                                 </div>
                                 <div className='row'>
-                                    <p className='col card-text text-secondary'>Estado DEA</p>
+                                    <p className='col-6 text-start card-text text-secondary'>Estado DEA</p>
                                     <p className='col card-text text-secondary-emphasis'>valor</p>
-                                    <p className='col card-text text-secondary-emphasis'>RL</p>
+                                    <div className='col'>
+                                        <p className='card-text badge bg-success'>RL</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
