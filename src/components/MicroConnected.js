e8d6e26dc16e802e1e8cbca6b3abf2d695c60056
Julian <jm.ord98@gmail.com>
2023-10-16 19:40:53 -0300

    toastr changes

--- src/components/MicroConnected.js
+++ src/components/MicroConnected.js
@@ -8,10 +8,10 @@ export class MicroConnected extends Component {
 
     static toastProperties = {
         position: "bottom-right",
-        autoClose: 5000,
+        autoClose: 2500,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: true,
+        pauseOnHover: false,
         draggable: true,
         progress: undefined,
         theme: "colored",
